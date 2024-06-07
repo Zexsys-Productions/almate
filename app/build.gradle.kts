9158bf800e57a6500a2b92049717c4742c54695b
Ryan Wiguna <ryan0702.rw@gmail.com>
2024-06-07 16:02:11 +0700

    Test CI/CD (versioncode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.zexsys.almate"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "v0.02-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
